b857d569c67440cc10b8c0d73921bfd37c99c2cc
Denisse 'Scar' Del Monte <d.delmonte@yahoo.com>
2022-09-21 23:42:12 -0400

    cleaned up code

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,31 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+// Add event listeners to the gallery buttons
+document.getElementById('mainBtn').addEventListener('click',showMain)
+document.getElementById('kidsBtn').addEventListener('click',showKids)
+
+let kidsGal = document.getElementById('kidsGallery');
+let mainGal = document.getElementById('mainGallery');
+
+function showMain(){
+    if (mainGal.style.display === 'flex'){
+        mainGal.style.display = 'none';
+    } else {
+        kidsGal.style.display = 'none';
+        mainGal.style.display = 'flex';
+    }
+}
+
+function showKids(){
+    if (kidsGal.style.display === 'flex'){
+        kidsGal.style.display = 'none';
+    } else {
+        mainGal.style.display = 'none';
+        kidsGal.style.display = 'flex';
+    }
+}
+
 (function($) {
 
 	var	$window = $(window),
@@ -34,31 +59,31 @@
 	// Nav.
 
 		// Title Bar.
-			$(
-				'<div id="titleBar">' +
-					'<a href="#navPanel" class="toggle"></a>' +
-				'</div>'
-			)
-				.appendTo($body);
+			// $(
+			// 	'<div id="titleBar">' +
+			// 		'<a href="#navPanel" class="toggle"></a>' +
+			// 	'</div>'
+			// )
+			// 	.appendTo($body);
 
 		// Panel.
-			$(
-				'<div id="navPanel">' +
-					'<nav>' +
-						$('#nav').navList() +
-					'</nav>' +
-				'</div>'
-			)
-				.appendTo($body)
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					side: 'left',
-					target: $body,
-					visibleClass: 'navPanel-visible'
-				});
+			// $(
+			// 	'<div id="navPanel">' +
+			// 		'<nav>' +
+			// 			$('#nav').navList() +
+			// 		'</nav>' +
+			// 	'</div>'
+			// )
+			// 	.appendTo($body)
+			// 	.panel({
+			// 		delay: 500,
+			// 		hideOnClick: true,
+			// 		hideOnSwipe: true,
+			// 		resetScroll: true,
+			// 		resetForms: true,
+			// 		side: 'left',
+			// 		target: $body,
+			// 		visibleClass: 'navPanel-visible'
+			// 	});
 
 })(jQuery);
